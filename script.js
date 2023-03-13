fa46e950886025387cf60bdc7fa9fc221fea075f
Thioro Faye <fayethioro1998@gmail.com>
2023-03-13 08:04:49 +0000

    projet 4 partie JS avec notification et animation

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ let passwordGenerator = document.querySelector("#password-generator")
 let bloc;
 // console.log(tabcheck);
 
-function getRandomLower() {
-    return console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 97))
-  }
+// function getRandomLower() {
+//     return console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 97))
+//   }
 
-getRandomLower()
+// getRandomLower()
 
 
 
